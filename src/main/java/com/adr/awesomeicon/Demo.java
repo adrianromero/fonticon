e2f3d27add14d836eadd18bf3108f8b0a50b2e31
Adrián Romero <adrianromero@users.noreply.github.com>
2014-09-30 23:22:45 +0200

    new examples

--- src/main/java/com/adr/awesomeicon/Demo.java
+++ src/main/java/com/adr/awesomeicon/Demo.java
@@ -20,6 +20,7 @@ package com.adr.awesomeicon;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -36,12 +37,16 @@ public class Demo extends Application {
                 new Icon(AwesomeIcon.ANDROID, 48.0, "fa-iconred", "fa-shadow"),
                 new Icon(AwesomeIcon.BANK, 48.0, "fa-iconblue", "fa-shadow"),
                 new Icon(AwesomeIcon.FILTER, 48.0, "fa-icongreen", "fa-shadow"),
+                new Button(null, new Icon(AwesomeIcon.APPLE, 48.0, "fa-iconwhite", "fa-shadow")),
                 new Button("test", new Icon(AwesomeIcon.BELL, 48.0, "fa-iconblue", "fa-plain", "fa-dropshadow")),
                 new Button("test", new StackPane(
                         new Icon(AwesomeIcon.SQUARE, 48.0, "fa-icondarkblue", "fa-stack-base"),
                         new Icon(AwesomeIcon.REFRESH, 32.0, "fa-iconwhite", "fa-shadow"))),
-                new Button("test", new Icon(AwesomeIcon.BOMB, 48.0, "fa-iconred", "fa-shadow")));
-        
+                new Button("test", new Icon(AwesomeIcon.BOMB, 48.0, "fa-iconred", "fa-shadow")),
+                new Button("test", new StackPane(                        
+                        new Icon(AwesomeIcon.CALENDAR_O, 48.0, "fa-icondarkblue"),
+                        new Label("31"))));
+
         Scene scene = new Scene(pane);        
         scene.getStylesheets().add(AwesomeStyle.BASE.getPath());
 
