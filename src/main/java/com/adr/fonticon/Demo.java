05e6e426ca5d3f31bfd85bec1561f349f987cf28
Adrián Romero <adrianromero@users.noreply.github.com>
2020-01-01 18:20:29 +0100

    Encapsulate Demo Details component

--- src/main/java/com/adr/fonticon/Demo.java
+++ src/main/java/com/adr/fonticon/Demo.java
@@ -16,10 +16,6 @@
 //     limitations under the License.
 package com.adr.fonticon;
 
-import com.adr.fonticon.decorator.LightOff;
-import com.adr.fonticon.decorator.LightOn;
-import com.adr.fonticon.decorator.ShadowHigh;
-import com.adr.fonticon.decorator.ShadowHole;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -28,13 +24,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -53,30 +46,9 @@ public class Demo extends Application {
         stage.show();
     }
 
-    private EventHandler<ActionEvent> displayDetails(IconFont icon, Label title, HBox sizer, HBox sizer2) {
+    private EventHandler<ActionEvent> displayDetails(IconFont icon, DemoDetails details) {
         return (ActionEvent ev) -> {
-            title.setText(icon.toString());
-            sizer.getChildren().clear();
-            sizer.getChildren().addAll(
-                    IconBuilder.create(icon, 12.0).styleClass("icon-size").build(),
-                    IconBuilder.create(icon, 16.0).styleClass("icon-size").build(),
-                    IconBuilder.create(icon, 20.0).styleClass("icon-size").build(),
-                    IconBuilder.create(icon, 24.0).styleClass("icon-size").build(),
-                    IconBuilder.create(icon, 28.0).styleClass("icon-size").build(),
-                    IconBuilder.create(icon, 32.0).styleClass("icon-size").build(),
-                    IconBuilder.create(icon, 40.0).styleClass("icon-size").build(),
-                    IconBuilder.create(icon, 48.0).styleClass("icon-size").build(),
-                    IconBuilder.create(icon, 52.0).styleClass("icon-size").build(),
-                    IconBuilder.create(icon, 56.0).styleClass("icon-size").build()
-            );
-            sizer2.getChildren().clear();
-            sizer2.getChildren().addAll(
-                    IconBuilder.create(icon, 52.0).color(Color.LIGHTGRAY).shine(Color.YELLOW).build(),
-                    IconBuilder.create(icon, 52.0).apply(new LightOn(Color.AQUA)).build(),
-                    IconBuilder.create(icon, 52.0).apply(new LightOff()).build(),
-                    IconBuilder.create(icon, 52.0).color(Color.RED).apply(new ShadowHigh(Color.BLUE)).build(),
-                    IconBuilder.create(icon, 52.0).apply(new ShadowHole(Color.AZURE)).build()
-            );
+            details.displayDetails(icon);
         };
     }
 
@@ -91,20 +63,13 @@ public class Demo extends Application {
         p.setFocusTraversable(false);
         VBox.setVgrow(p, Priority.ALWAYS);
 
-        Label title = new Label();
-        title.getStyleClass().add("title-icon");
+        DemoDetails details = new DemoDetails();
 
-        HBox sizer = new HBox();
-        sizer.getStyleClass().add("sizer-icon");
-
-        HBox sizer2 = new HBox();
-        sizer2.getStyleClass().add("sizer-icon");
-
-        VBox box = new VBox(p, title, sizer, sizer2);
+        VBox box = new VBox(p, details.getNode());
 
         boolean clean = true;
         for (IconFont icon : icons) {
-            EventHandler<ActionEvent> handler = displayDetails(icon, title, sizer, sizer2);
+            EventHandler<ActionEvent> handler = displayDetails(icon, details);
             if (clean) {
                 clean = false;
                 handler.handle(new ActionEvent());
---
+++ src/main/java/com/adr/fonticon/DemoDetails.java
@@ -0,0 +1,81 @@
+//    FontIcon is a JavaFX library to use FontIcons
+//    Copyright (C) 2020 Adrián Romero Corchado.
+//
+//    This file is part of FontIcon
+//
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+package com.adr.fonticon;
+
+import com.adr.fonticon.decorator.LightOff;
+import com.adr.fonticon.decorator.LightOn;
+import com.adr.fonticon.decorator.ShadowHigh;
+import com.adr.fonticon.decorator.ShadowHole;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author adrian
+ */
+public class DemoDetails {
+
+    private final Label title;
+    private final HBox sizer;
+    private final HBox sizer2;
+    private final VBox box;
+
+    public DemoDetails() {
+        title = new Label();
+        title.getStyleClass().add("title-icon");
+
+        sizer = new HBox();
+        sizer.getStyleClass().add("sizer-icon");
+
+        sizer2 = new HBox();
+        sizer2.getStyleClass().add("sizer-icon");
+
+        box = new VBox(title, sizer, sizer2);
+    }
+
+    public Node getNode() {
+        return box;
+    }
+
+    public void displayDetails(IconFont icon) {
+        title.setText(icon.toString());
+        sizer.getChildren().clear();
+        sizer.getChildren().addAll(
+                IconBuilder.create(icon, 12.0).styleClass("icon-size").build(),
+                IconBuilder.create(icon, 16.0).styleClass("icon-size").build(),
+                IconBuilder.create(icon, 20.0).styleClass("icon-size").build(),
+                IconBuilder.create(icon, 24.0).styleClass("icon-size").build(),
+                IconBuilder.create(icon, 28.0).styleClass("icon-size").build(),
+                IconBuilder.create(icon, 32.0).styleClass("icon-size").build(),
+                IconBuilder.create(icon, 40.0).styleClass("icon-size").build(),
+                IconBuilder.create(icon, 48.0).styleClass("icon-size").build(),
+                IconBuilder.create(icon, 52.0).styleClass("icon-size").build(),
+                IconBuilder.create(icon, 56.0).styleClass("icon-size").build()
+        );
+        sizer2.getChildren().clear();
+        sizer2.getChildren().addAll(
+                IconBuilder.create(icon, 52.0).color(Color.LIGHTGRAY).shine(Color.YELLOW).build(),
+                IconBuilder.create(icon, 52.0).apply(new LightOn(Color.AQUA)).build(),
+                IconBuilder.create(icon, 52.0).apply(new LightOff()).build(),
+                IconBuilder.create(icon, 52.0).color(Color.RED).apply(new ShadowHigh(Color.BLUE)).build(),
+                IconBuilder.create(icon, 52.0).apply(new ShadowHole(Color.AZURE)).build()
+        );
+    }
+}
